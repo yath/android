1aa410443ccee272098bdaaa2d41270b87845f02
Stefan Tomanek <stefan@pico.ruhr.de>
2010-05-19 22:48:48 +0200

    autodetect mountpoint

--- adfree.sh
+++ adfree.sh
@@ -28,20 +28,23 @@ fi
 
 HOSTFILE=${1:-/etc/hosts}
 
+# detect mountpoint of the host file
+ETC_MOUNT="$(df -P "$HOSTFILE" | awk 'NR==2 { print $6 }')"
+
 #################################################
 set -e
 
 echo "starting adfree v${VERSION}..."
 
 is_rw() {
-    awk '$2 == "/system" && $4 ~/(^|,)rw(,|$)/ {RW=1} END{exit ! RW}' /proc/mounts
+    awk -vD="$ETC_MOUNT" '$2 == D && $4 ~/(^|,)rw(,|$)/ {RW=1} END{exit ! RW}' /proc/mounts
 }
 
 REMOUNTED=0
-is_rw || { mount /system -o remount,rw; REMOUNTED=1; }
+is_rw || { mount "$ETC_MOUNT" -o remount,rw; REMOUNTED=1; }
 
 # better be safe than sorry...
-is_rw || { echo "Unable to gain write access to /system, aborting" >&2; exit 1; }
+is_rw || { echo "Unable to gain write access to $ETC_MOUNT, aborting" >&2; exit 1; }
 
 # remove old adfree entries
 echo "Removing ADFREE entries from $HOSTFILE"
@@ -66,7 +69,7 @@ if [ "$REMOVE_ONLY" -ne 1 ]; then
     } 3>&1 >> "$HOSTFILE"
 fi
 
-# mount /system read-only if it was at the beginning
-[ "$REMOUNTED" -eq 1 ] && mount /system -o remount,ro
+# mount directory read-only if it was at the beginning
+[ "$REMOUNTED" -eq 1 ] && mount "$ETC_MOUNT" -o remount,ro
 
 echo "done."
