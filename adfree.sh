b9418c9d3721d6ca01afa1d927379b962e5165f9
Sebastian Schmidt <yath@yath.de>
2010-10-05 20:59:32 +0200

    Temporarily create resolv.conf if nonexistant, busybox' wget demands it

--- adfree.sh
+++ adfree.sh
@@ -28,6 +28,8 @@ fi
 
 HOSTFILE=${1:-/etc/hosts}
 
+RESOLVCONF="/etc/resolv.conf"
+
 B="busybox"
 
 # detect mountpoint of the host file
@@ -43,11 +45,25 @@ is_rw() {
     $B awk -vD="$ETC_MOUNT" '$2 == D && $4 ~/(^|,)rw(,|$)/ {RW=1} END{exit ! RW}' /proc/mounts
 }
 
+check_resolvconf() {
+    [ -e "$RESOLVCONF" ] && return
+    i=1
+    while :; do
+        dns=$(getprop net.dns$i)
+        [ -z "$dns" ] && break
+        RESOLVCONF_CREATED=1
+        echo "nameserver $dns" >> "$RESOLVCONF"
+        i=$((i+1))
+    done
+}
+
 REMOUNTED=0
 is_rw || { $B mount "$ETC_MOUNT" -o remount,rw; REMOUNTED=1; }
 
 # better be safe than sorry...
 is_rw || { echo "Unable to gain write access to $ETC_MOUNT, aborting" >&2; exit 1; }
+
+check_resolvconf
 
 # remove old adfree entries
 echo "Removing ADFREE entries from $HOSTFILE"
@@ -74,5 +90,7 @@ fi
 
 # mount directory read-only if it was at the beginning
 [ "$REMOUNTED" -eq 1 ] && $B mount "$ETC_MOUNT" -o remount,ro
+
+[ "$RESOLVCONF_CREATED" -eq 1 ] && rm "$RESOLVCONF"
 
 echo "done."
